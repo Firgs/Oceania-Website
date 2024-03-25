53e8527442cc1538461c6cf3eb752d5d7dc74c87
Ficodes <131626863+Firgs@users.noreply.github.com>
2024-03-25 12:22:24 +1100

    Add files via upload

---
+++ questions_c.js
@@ -0,0 +1,116 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What percentage of the Earth's surface is covered by oceans?",
+        answer: "C) 71%.",
+        options: [
+            "A) 51%.",
+            "B) 61%.",
+            "C) 71%.",
+            "D) 81%."
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the most common type of trash found in the oceans?",
+        answer: "D) All of the above.",
+        options: [
+            "A) Plastic bottles and food wrappers.",
+            "B) Cigarette butts.",
+            "C) Fishing nets.",
+            "D) All of the above."
+        ]
+    },
+    {
+        numb: 3,
+        question: "The Great Barrier Reef is located in which ocean?",
+        answer: "C) Pacific Ocean.",
+        options: [
+            "A) Atlantic Ocean.",
+            "B) Indian Ocean.",
+            "C) Pacific Ocean.",
+            "D) Southern Ocean."
+        ]
+    },
+    {
+        numb: 4,
+        question: "What percentage of the world's oxygen is produced by the ocean?",
+        answer:"B) 50%.",
+        options: [
+            "A) 20%.",
+            "B) 50%.",
+            "C) 70%.",
+            "D) 85%."
+            ]
+    },
+    {
+        numb: 5,
+        question: "What are microplastics?",
+        answer: "B) Tiny plastic particles smaller than 5mm, often resulting from the breakdown of larger plastics.",
+        options: [
+            "A) Small, visible pieces of plastic used in manufacturing.",
+            "B) Tiny plastic particles smaller than 5mm, often resulting from the breakdown of larger plastics.",
+            "C) Natural, biodegradable plastic particles found in the ocean.",
+            "D) Large plastic items that are found in household waste."
+            ]
+    },  
+    {
+        numb: 6,
+        question: "Which of these are sources of microplastics in the ocean?",
+        answer: "D) All of the above.",
+        options: [
+            "A) Breakdown of larger plastic items like bottles and bags.",
+            "B) Synthetic textile fibers from washing clothes.",
+            "C) Microbeads from cosmetic products like toothpaste and exfoliating masks.",
+            "D) All of the above."
+            ]
+    }, 
+    {
+        numb: 7,
+        question: "How does plastic pollution contribute to climate change?",
+        answer: "B) By emitting greenhouse gases as it degrades.",
+        options: [
+            "A) By absorbing sunlight.",
+            "B) By emitting greenhouse gases as it degrades.",
+            "C) By blocking solar radiation.",
+            "D) It does not contribute."
+
+            ]
+    }, 
+    {
+        numb: 8,
+        question: "Microplastics can enter the food chain through which process?",
+        answer: "B) Ingestion by plankton and small fish, eventually reaching larger animals.",
+        options: [
+            "A) Photosynthesis in aquatic plants.",
+            "B) Ingestion by plankton and small fish, eventually reaching larger animals.",
+            "C) Absorption through fish gills directly from water.",
+            "D) Chemical reactions in seawater."
+
+            ]
+    },   
+    {
+        numb: 9,
+        question: "What are some potential consequences of plastic ingestion in humans?",
+        answer: "D) All of the above.",
+        options: [
+            "A) Disruption of hormonal systems.",
+            "B) Increased risk of certain cancers.",
+            "C) Digestive tract blockages or injuries.",
+            "D) All of the above."
+        ] 
+    },       
+    {
+        numb: 10,
+        question: "What is the long-term environmental impact of microplastics?",
+        answer: "C) They persist for centuries, posing a continuous threat to ecosystems.",
+        options: [
+            "A) They eventually dissolve harmlessly.",
+            "B) They create new habitats for small organisms.",
+            "C) They persist for centuries, posing a continuous threat to ecosystems.",
+            "D) They convert into natural gas and escape into the atmosphere."
+
+            ]
+    },]
+
+    
